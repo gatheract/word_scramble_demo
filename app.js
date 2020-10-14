ae8e5aa8decfb5a0b64918f7c65570a17a1abb4a
Joe Crop <joecrop@Joes-MacBook-Pro.local>
2020-10-13 22:25:37 -0700

    removed nuneeded code

--- app.js
+++ app.js
@@ -7,23 +7,15 @@ words = [
 ];
 
 // Take the new word and update the state with that word and the shuffled letter for the user to guess
-function updateLetters(isNew, word, letters) {
+function updateLetters(word) {
   state.word = [];
   state.letters = [];
   // Vue.nextTick is used to prevent a key-not-found error if the state changes
   // before Vue has a chance to react to the variables being cleared on the lines above
   Vue.nextTick(function () {
-    if (isNew) {
-      // isNew=1 when user clicked refresh button
       state.goal = word;
       // randomize the display of the letter choices
       state.letters = state.goal.split("").sort(() => Math.random() - 0.5);
-    } else {
-      // message from another user
-      state.word = word;
-      // We don't mix the letters because the letters have already been mixed by the user that sent them
-      state.letters = letters;
-    }
   });
 }
 
@@ -36,7 +28,8 @@ state = {
   letters: [],
   // A function called when the user clicks the 'new word' button in the top right corner
   refresh: function () {
-    updateLetters(1, words[Math.floor(Math.random() * words.length)].toUpperCase());
+    // Pick a random word to update the game with
+    updateLetters(words[Math.floor(Math.random() * words.length)].toUpperCase());
     Vue.nextTick(function () {
       // We send the guess inside a nextTick to ensure the new data is valid before sending it out
       sendGuess();
@@ -51,7 +44,7 @@ new Vue({
   },
   mounted: function () {
     // When the page first loads, initialize with a new random word
-    updateLetters(1, words[Math.floor(Math.random() * words.length)].toUpperCase());
+    updateLetters(words[Math.floor(Math.random() * words.length)].toUpperCase());
   },
   computed: {
     // this method is called vy VUE to set a class in the main app that sets the 
